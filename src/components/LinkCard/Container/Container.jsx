86a76a108a34ede75d62368d50284e73a1dea876
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-12 19:25:27 -0300

    :sparkles: main component & linkCard component

---
+++ src/components/LinkCard/Container/Container.jsx
@@ -0,0 +1,11 @@
+import "./Container.css";
+
+function Container() {
+  return (
+    <div className="container">
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+export default Container;
---
+++ src/components/LinkCard/LinkCard.jsx
@@ -0,0 +1,7 @@
+import "./LinkCard.css";
+
+function LinkCard() {
+  return <div>LinkCard</div>;
+}
+
+export default LinkCard;
