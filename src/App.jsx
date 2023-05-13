ffeec63d8adb4588837dcdcfa038d2ee0e16fa70
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-13 10:19:04 -0300

    import/style profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Container from "./components/LinkCard/ProfileCard/ProfileCard";
+import Container from "./components/ProfileCard/ProfileCard";
+
+import pfp from "./assets/img/pfp.png";
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Container />
+      <Container img={pfp} alt={"Foto do Marcelo sorrindo"} />
     </div>
   );
 }
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,7 +3,11 @@ import "./ProfileCard.css";
 function Container({ img, alt }) {
   return (
     <div className="profile-card">
-      <img className="rounded-full" src={img} alt={alt} />
+      <img
+        className="h-40 w-40 rounded-full ring-2 ring-white"
+        src={img}
+        alt={alt}
+      />
     </div>
   );
 }
