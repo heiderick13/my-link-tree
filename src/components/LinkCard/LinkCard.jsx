7f7ce6ec4bad3b5052d9857b1ceb024ba032eacf
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-14 10:32:34 -0300

    linkCard structure

--- src/components/LinkCard/LinkCard.jsx
+++ src/components/LinkCard/LinkCard.jsx
@@ -1,7 +1,14 @@
 import "./LinkCard.css";
 
-function LinkCard() {
-  return <div>LinkCard</div>;
+function LinkCard({ icon, iconAlt, url, social }) {
+  return (
+    <div>
+      <a className="flex" href={url} target="_blank">
+        <img src={icon} alt={iconAlt} />
+        <p>{social}</p>
+      </a>
+    </div>
+  );
 }
 
 export default LinkCard;
